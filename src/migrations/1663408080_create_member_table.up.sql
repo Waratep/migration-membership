93d8ce391666d924872a832a17f53300f8c5f30d
Waratep Tubboon <waratep@sellsuki.com>
2022-10-16 17:36:24 +0700

    add member timestamp

--- src/migrations/1663408080_create_member_table.up.sql
+++ src/migrations/1663408080_create_member_table.up.sql
@@ -5,5 +5,7 @@ CREATE TABLE
         last_name VARCHAR (50) NOT NULL,
         phone VARCHAR (20) UNIQUE NOT NULL,
         email VARCHAR (50) UNIQUE NOT NULL,
-        address VARCHAR (200) NOT NULL
+        address VARCHAR (200) NOT NULL,
+        created_at TIMESTAMP,
+        updated_at TIMESTAMP
     );
